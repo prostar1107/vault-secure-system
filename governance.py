cc531af9805566975ad9f6a83660a95a5d0cca8f
prostar1107 <98318099+prostar1107@users.noreply.github.com>
2023-04-08 14:00:01 +0200

    Fix ERC-1155 contract interactions

--- governance.py
+++ governance.py
@@ -1,3 +1,4 @@
 // Fix issues with token vesting contracts
 // Add support for ERC-721A tokens
 // Fix errors in contract interaction with wallets
+// Optimize state channels for faster transactions
