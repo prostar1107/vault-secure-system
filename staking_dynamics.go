e9c0939d9b9f4bd1b90679c1de6d03e24bb6e7c1
prostar1107 <98318099+prostar1107@users.noreply.github.com>
2023-06-21 14:00:01 +0200

    Refactor multi-asset token management

--- staking_dynamics.go
+++ staking_dynamics.go
@@ -1,3 +1,4 @@
 // Fix issues with cross-chain transaction finality
 // Implement adaptive block size for Ethereum
 // Optimize gas cost in function execution
+// Add native support for staking rewards
