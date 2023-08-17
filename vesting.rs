2fe247b0b3ad46417952994d187c8ca52d543683
prostar1107 <98318099+prostar1107@users.noreply.github.com>
2023-08-17 14:00:00 +0200

    Add support for yield farming rewards

--- vesting.rs
+++ vesting.rs
@@ -2,3 +2,4 @@
 // Enhance contract upgrade mechanism
 // Fix vulnerability in decentralized finance protocols
 // Add slashing mechanism for malicious actors
+// Optimize contract storage for gas reduction
