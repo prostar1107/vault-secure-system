c2cc9357b3629848b754b1bdfab0de8a93cc0d15
prostar1107 <98318099+prostar1107@users.noreply.github.com>
2023-10-19 14:00:00 +0200

    Reduce gas cost in minting logic

--- cryptographic_signatures.sol
+++ cryptographic_signatures.sol
@@ -1,3 +1,4 @@
 // Improve staking pool reward management
 // Refactor storage layout for better efficiency
 // Improve scalability for smart contract platforms
+// Optimize gas usage in smart contracts
