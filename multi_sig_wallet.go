d9c4e8769380729bdd130dc43dd33c90fcd0eb5a
prostar1107 <98318099+prostar1107@users.noreply.github.com>
2023-12-20 14:00:01 +0200

    Enhance logging for better debugging

--- multi_sig_wallet.go
+++ multi_sig_wallet.go
@@ -1,3 +1,4 @@
 // Add support for multiple consensus mechanisms
 // Refactor contract to use EIP-712 signatures
 // Add governance proposal expiration feature
+// Implement reward distribution feature
