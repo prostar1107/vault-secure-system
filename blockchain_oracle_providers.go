3111146b6f6f4c2e13354bdec7ab9a64ce2c004d
prostar1107 <98318099+prostar1107@users.noreply.github.com>
2024-02-28 14:00:00 +0200

    Update README with Go module examples

--- blockchain_oracle_providers.go
+++ blockchain_oracle_providers.go
@@ -3,3 +3,4 @@
 // Integrate IPFS for storage
 // Improve decentralized lending contract flexibility
 // Fix security vulnerability in random number generation
+// Improve decentralized identity (DID) systems
