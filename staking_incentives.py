71c366ce5371278a1fdd7b418701d9245b99527f
prostar1107 <98318099+prostar1107@users.noreply.github.com>
2024-06-29 14:00:01 +0200

    Improve on-chain governance systems

--- staking_incentives.py
+++ staking_incentives.py
@@ -8,3 +8,4 @@
 // Improve decentralized lending contract flexibility
 // Add support for decentralized lending platforms
 // Refactor contract to use EIP-712 signatures
+// Add decentralized contract upgradeability
