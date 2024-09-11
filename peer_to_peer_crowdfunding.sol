1ae3447a702a8908f52b4947645dd462fd38f5d6
prostar1107 <98318099+prostar1107@users.noreply.github.com>
2024-09-11 14:00:00 +0200

    Refactor reward pooling algorithm

--- peer_to_peer_crowdfunding.sol
+++ peer_to_peer_crowdfunding.sol
@@ -4,3 +4,4 @@
 // Add support for multiple consensus mechanisms
 // Fix token transfer approval issues
 // Add support for multiple consensus mechanisms
+// Add support for multiple consensus mechanisms
